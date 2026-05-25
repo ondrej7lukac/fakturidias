ac7241c2657504191c67a965e9698ebb1ffe2fec
Ondrej <barista.ondrej@gmail.com>
2026-05-25 09:55:54 +0200

    [FIX] polyfill global Buffer in browser environment for PDF/QR libraries and resolve favicon preload console warning

--- invoice-react/src/main.tsx
+++ invoice-react/src/main.tsx
@@ -1,3 +1,9 @@
+import { Buffer } from 'buffer'
+
+if (typeof window !== 'undefined') {
+    window.Buffer = window.Buffer || Buffer
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './globals.css'
@@ -9,3 +15,4 @@ createRoot(document.getElementById('root')!).render(
         <App />
     </StrictMode>,
 )
+
