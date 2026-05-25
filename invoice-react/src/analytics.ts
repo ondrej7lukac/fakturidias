41bc70a5086fecfd089917182cf5eb7edcd94621
alphasr <nilaybaranwal@gmail.com>
2026-05-25 04:16:17 +0200

    [FIX] fixed deploy

--- invoice-react/src/analytics.ts
+++ invoice-react/src/analytics.ts
@@ -1,32 +1,19 @@
-declare global {
-    interface Window {
-        dataLayer: unknown[]
-        gtag: (...args: unknown[]) => void
-    }
-}
+import ReactGA from 'react-ga4'
 
 const GA_ID = import.meta.env.VITE_GA4_ID as string | undefined
 
 if (GA_ID) {
-    window.dataLayer = window.dataLayer || []
-    window.gtag = function gtag(...args: unknown[]) {
-        window.dataLayer.push(args)
-    }
+    ReactGA.initialize(GA_ID, {
+        gaOptions: { anonymizeIp: true },
+    })
 
-    window.gtag('js', new Date())
-    window.gtag('consent', 'default', {
+    ReactGA.gtag('consent', 'default', {
         analytics_storage: 'denied',
         ad_storage: 'denied',
         ad_user_data: 'denied',
         ad_personalization: 'denied',
         wait_for_update: 500,
     })
-    window.gtag('config', GA_ID, { anonymize_ip: true })
-
-    const script = document.createElement('script')
-    script.async = true
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
-    document.head.appendChild(script)
 }
 
 export {}
